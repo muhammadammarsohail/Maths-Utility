87481e47834dff4bff1db8daeed9a662b79f4b9d
Ammar Sohail <muhammadammarsohail@gmail.com>
2019-04-28 22:50:16 +0500

    Add project files.

---
+++ NotEvenException.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EvenException
+{
+    class NotAnEvenException : ApplicationException
+    {
+        NotAnEvenException(string errorMessage) : base(errorMessage)
+        {
+            
+        } 
+
+        
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EvenException
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                double fNum = Convert.ToInt32(Console.ReadLine()), sNum = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine(Divide(fNum,sNum));
+            }
+            catch(DivideByZeroException excD)
+            {
+                Console.WriteLine(excD.Message);
+            }
+            catch(NotAnEvenException excE)
+            {
+                Console.WriteLine(excE.Message);
+            }
+        }
+
+        static double Divide(double firstNumber, double secondNumber)
+        {
+            if (secondNumber == 0)
+                throw new DivideByZeroException();
+
+                if (firstNumber % 2 != 0 || secondNumber % 2 != 0)
+                    throw new ApplicationException("One of the parameters is not even!");
+
+            return firstNumber / secondNumber;
+
+            
+            
+        }
+    }
+}
